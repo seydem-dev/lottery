f74adfbf7dc51ad4bbbaac1edd0d397296974bfc
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-15 20:30:50 +0200

    Add Lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,5 +4,42 @@ pragma solidity ^0.8.0;
 
 contract Lottery {
 
+    uint256 public lotteryId;
 
+    address public immutable owner;
+    address payable[] public players;
+
+    mapping (uint256 => address payable) public lotteryHistory;
+
+    constructor() {
+        owner = msg.sender;
+        lotteryId = 1;
+    }
+
+    function enter() external payable {
+        require(msg.value >= 0.01 ether, "Need to pay at least 0.01 ETH");
+        players.push(payable(msg.sender));
+    }
+
+    function getRandomNumber() public view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(owner, block.timestamp)));
+    }
+
+    function pickWinner() external {
+        require(msg.sender == owner, "Not owner");
+        uint256 playersLength = players.length;
+        uint256 index = getRandomNumber() % playersLength;
+        players[index].transfer(address(this).balance);
+        lotteryHistory[lotteryId] = players[index];
+        lotteryId++;
+        players = new address payable[](0);
+    }
+
+    function getPlayers() external view returns (address payable[] memory) {
+        return players;
+    }
+
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
 }
