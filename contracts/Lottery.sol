cd68abaef7d756e95215866de609c8331492142e
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-17 23:31:30 +0200

    Structure code

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 
 contract Lottery {
 
+    address public immutable owner;
+
     uint256 public lotteryId = 1;
 
-    address public immutable owner;
-    
     address payable[] public players;
 
     mapping (uint256 => address payable) public lotteryHistory;
